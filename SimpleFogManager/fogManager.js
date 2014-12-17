4e626e81d93410e49f7bbe86fab087eda8286970
Simon Mayer <simon.mayer@inf.ethz.ch>
2014-12-16 19:31:27 -0800

    Implemented basic functionality for Fog Manager

--- SimpleFogManager/fogManager.js
+++ SimpleFogManager/fogManager.js
@@ -1,5 +1,31 @@
-// TODO create application at Pi
+piURL = 'http://128.32.211.227:8080/app';
+selfURL = 'http://127.0.0.1:8080/app';
+
+var rest = require('restler');
+
+function startAppAt(appName, targetURL) {
+	rest.post(targetURL, {
+		name : appName,
+		headers : {
+			'Content-Type' : 'application/json'
+		}
+	}
+	).on('complete', function(data, response) {
+		console.log('POST. Received response code ' + response.statusCode);
+	});
+}
+
+function stopAppAt(appName, targetURL) {
+	rest.delete(targetURL)).on('complete', function(data, response) {
+		console.log('DELETE. Received response code ' + response.statusCode);
+	});
+}
+
+// Create application at Pi
+createAppAt('cpuMonitor', piURL);
 
 // TODO Move application to Local
+
+//TODO Move application back to Pi
 
 // TODO CLI?
